2cb9675d635905f1d29d93b4a7b79f051b25c52c
odlavir <lavir.andrade@gmail.com>
2023-08-28 17:04:10 -0300

    Enviando a primeira atividade.

---
+++ atividade01.py
@@ -0,0 +1,12 @@
+"""
+1. Crie um programa que peça um número e mostre o seu sucessor e antecessor
+"""
+
+numeroRecebido = int(input("Digite um número: "))
+
+numeroAntecessor = numeroRecebido - 1
+numeroSucessor = numeroRecebido + 1
+
+print(f"O número recebido foi: {numeroRecebido}")
+print(f"O número antecessor é: {numeroAntecessor}")
+print(f"O número sucessor é: {numeroSucessor}")
